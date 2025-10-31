501f683ee380b795e6557459b95bda97b3244080
jiangdengke <jiang1728439852@gmail.com>
2025-10-31 01:52:51 +0000

    chore: lock dependencies and prep for gradle build

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,33 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const callerName = api.caller?.((caller) => caller?.name) ?? 'unknown';
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['module-resolver', {
-        alias: {
-          '@': './src'
-        }
-      }],
-      ['tamagui', {
-        config: './tamagui.config.ts',
-        components: ['tamagui'],
-        disableExtraction: process.env.NODE_ENV === 'development'
-      }],
-      'react-native-reanimated/plugin'
-    ]
+    plugins: (() => {
+      const plugins = [
+        ['module-resolver', {
+          alias: {
+            '@': './src'
+          }
+        }],
+        ['tamagui', {
+          config: './tamagui.config.ts',
+          components: ['tamagui'],
+          disableExtraction: process.env.NODE_ENV === 'development'
+        }],
+      ];
+
+      const disallowedCallers = new Set(['config-plugins', 'unknown']);
+      const shouldEnableReanimated =
+        process.env.DISABLE_REANIMATED_PLUGIN !== '1' && !disallowedCallers.has(callerName);
+
+      if (shouldEnableReanimated) {
+        plugins.push('react-native-reanimated/plugin');
+      }
+
+      return plugins;
+    })()
   };
 };
